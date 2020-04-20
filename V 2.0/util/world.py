42d18c360041395666a7e3c804319dac93cd523a
Federico <45139706+fededev01@users.noreply.github.com>
2020-04-20 23:01:37 +0200

    updated info about world cases

--- V 2.0/util/world.py
+++ V 2.0/util/world.py
@@ -1,10 +1,10 @@
-COMg21m01 = []
+COMg21m01 = [6,282] #6 deaths, 278 cases
 
-COMg22m01 = []
+COMg22m01 = [6,314]
 
-COMg23m01 = []
+COMg23m01 = [17,581]
 
-COMg24m01 = []
+COMg24m01 = [25,846]
 
-COMg25m01 = []
+COMg25m01 = [41,1320]
 
