3438a1c1eb3af76902fbcca4e5f6c2f53ed95e08
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-02 19:40:02 +0200

    started to work at "nuovi_casi"

--- docs/main.py
+++ docs/main.py
@@ -1,2 +1,23 @@
-from Spagna import *
-from Italia import *
+from pip._vendor import requests
+import json
+import datetime
+from datetime import timedelta
+
+
+url = 'https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json'
+reponse = requests.get(url)
+dati = json.loads(reponse.content)
+
+
+data = "2020/01/22"
+nation = "Italy"
+
+data_2 = data + timedelta(days = 1)
+
+nuovi_casi = []
+
+for righe in dati[nation]:
+   x = dati[data]["confirmed"]
+   y = dati[data_2]["confirmed"]
+   z = y - x
+   nuovi_casi.append(z)
