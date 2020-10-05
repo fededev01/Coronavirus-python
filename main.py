bdf2e55aff82a4b71457219d23dc9923166dcb30
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-05 22:28:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-from Spagna import *
-from Italia import *
+import requests
+import json
+
+url = 'https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json'
+reponse = requests.get(url)
+dati = json.loads(reponse.content)
+
+nation = str(input("Insert here a nation"))
+cosa = str(input("Insert here what do you want receive"))
+
+fir = []
+for info in dati[nation]:
+  fir.append(info[cosa])
+
+print(fir)
