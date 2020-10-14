4de80bfc933293c82a1ac50be4e4c96ad354ac09
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-15 00:14:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import requests
 import json
+import datetime
 
 url = 'https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json'
 reponse = requests.get(url)
 dati = json.loads(reponse.content)
 
-nation = str(input("Insert here a nation"))
-cosa = str(input("Insert here what do you want receive"))
+nation = str(input("Insert here a nation\n"))
+cosa = str(input("Insert here what do you want receive\n"))
 
-fir = []
-for info in dati[nation]:
-  fir.append(info[cosa])
+resp = dati[nation]
+data = []
+for righe in dati[nation]:
+  data.append(righe["date"])
 
-print(fir)
+if cosa == "nuovi casi":
+  fir = []
+  for y in dati[nation]:
+    fir.append(y["confirmed"])
+  nuovi_casi = [0]  
+  while len(fir) > 0:
+    x = fir[0]
+    z = fir[1] - x
+    nuovi_casi.append(z)
+    del fir[0]
+    if len(fir) == 1:
+      break
+#  for righe in resp:
+#    data = datetime.datetime.strptime(righe['date'], "%Y-%m-%d")
+#    strdata = data.strftime("%d/%m/%Y")
+  while len(nuovi_casi) > 0:
+        print(data[0], nuovi_casi[0])
+        del data[0]
+        del nuovi_casi[0]
+        if len(nuovi_casi) == 0:
+          break
+elif cosa == "confirmed" or "deaths" or "recovered":
+  for info in dati[nation]:
+    data = datetime.datetime.strptime(info['date'], "%Y-%m-%d")
+    strdata = data.strftime("%d/%m/%Y")
+    print(strdata, info[cosa])
+else:
+  print("idk")  
