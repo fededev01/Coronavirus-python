0ee4ac891ddb8dfa0da747f409b3280db36e9693
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-16 09:28:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import requests
 import json
 import datetime 
+import pandas as pd
 
 url = 'https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json'
 reponse = requests.get(url)
 dati = json.loads(reponse.content)
 
 nation = "Italy"
-da_strp = "01/02/2020"
-a_strp = "29/02/2020"
-da = datetime.datetime.strptime(da_strp, "%d/%m/%Y")
-a = datetime.datetime.strptime(a_strp, "%d/%m/%Y")
+da = "01/02/2020"
+a = "29/02/2020"
 
 days = []   
 fir = []
+oth = []
 for info in dati[nation]:
   fir.append(info["confirmed"])
   days.append(info["date"])
+  oth.append(info)
   strdata = datetime.datetime.strptime(info['date'], "%Y-%m-%d")
   data = (strdata.strftime("%d/%m/%Y"))
   date_object = datetime.datetime.strptime(data, "%d/%m/%Y")
    
 results = []   
-for i in fir[da:a]:
+for i in range(fir[da:a]):
     results.append(i)
     print(results)
-
