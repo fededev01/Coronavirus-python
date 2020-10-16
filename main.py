64024444538de7dc678c0525a2178f66646155c7
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-16 15:12:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 import numpy as np
 
 url = 'https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json'
@@ -7,24 +8,21 @@ reponse = requests.get(url)
 dati = json.loads(reponse.content)
 
 nation = "Italy"
+cosa = "confirmed"
+
+data = []
+for righe in dati[nation]:
+  strdata = datetime.datetime.strptime(righe['date'], "%Y-%m-%d")
+  data.append(strdata.strftime("%d/%m/%Y"))
+
+fir = [] 
+for i in dati[nation]:
+  fir.append(i[cosa])
    
-fir = []
-for info in dati[nation]:
-  fir.append(info["confirmed"])  
+sec = np.array([fir[0:-1]])
+ter = np.array([fir[1:len(fir)]])
+#fourth = np.array([ter])
+data_np = np.array(data)
 
-nuovi = np.array([0]) 
-list = [0]
- 
-while len(fir) > 0:
-  z = fir[1] - fir[0]
-  list.append(z)
-  np.delete(fir, [0])
-  del fir [0]
-  if len(fir) == 1:
-    break
-
-
-for cat in enumerate(list, 1):
-  print(cat)
-
-
+nuovi_casi = ter - sec
+print(nuovi_casi)
