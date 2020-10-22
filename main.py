0b6255c6d9904a569c6e270b24147881cd6fb9e9
Federico <45139706+fededev01@users.noreply.github.com>
2020-10-22 08:52:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,25 +19,55 @@ for i in dati[nation]:
   rec.append(i["recovered"])
   mor.append(i["deaths"])  
 
+#cambiare nome variabili
 sec = np.array([conf])
 ter = conf[1: len(conf)]
 ter.append(conf[-1])
-quar = np.array([ter])
-nuovi_casi = ter - sec
+quar = np.array([ter]) #modificare 
+nuovi_casi = ter - sec #sostituire ter con quar
 
-nuovi = nuovi_casi.reshape(len(mor), 1)
+cinq = np.array([mor])
+sei = mor[1: len(mor)]
+sei.append(mor[-1])
+set = np.array([sei])
+nuovi_morti = set - cinq
+
+otto = np.array([rec])
+nove = rec[1: len(rec)]
+nove.append(rec[-1])
+dieci = np.array([nove])
+nuovi_ricoverati = dieci - otto
+#fine variabili da cambiare
+
+nuovi_c = nuovi_casi.reshape(len(mor), 1)
+nuovi_m = nuovi_morti.reshape(len(mor), 1)
+nuovi_r = nuovi_ricoverati.reshape(len(mor), 1)
+
 dates = pd.date_range('20200122', periods = len(rec))
 df = pd.DataFrame(index = dates)
 df['recovered'] = rec
 df['confirmed'] = conf
 df['deaths'] = mor
-df['nuovi casi'] = nuovi
+df['nuovi casi'] = nuovi_c
+df['nuovi morti'] = nuovi_m
+df['nuovi dimessi'] = nuovi_r
 
-if cosa == "nuovi casi" or "confirmed" or "deaths" or "recovered":
+if cosa == "nuovi casi" or "confirmed" or "deaths" or "recovered" or "nuovi morti" or "nuovi dimessi":
   try:
     print(df[cosa]) 
     plt.plot(df[cosa])
     plt.show()
+    if cosa == "nuovi casi":
+      n = np.sum(nuovi_c)
+      print("La media di nuovi casi è: " + n / len(conf))
+    elif cosa == "nuovi morti":
+      n = sum(nuovi_m)
+      print("La media di nuovi morti è: " + n / len(conf))
+    elif cosa == "nuovi dimessi":
+      n = sum(nuovi_r)
+      print("La media di nuovi dimessi è: " + n / len(conf))
+    else:
+      pass    
   except KeyError:
     if cosa == "data":
       giorno = str(input("Insert here your day\n"))
